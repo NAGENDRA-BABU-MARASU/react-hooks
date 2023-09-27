a6a3a305bccab27bd4016140743db7be7607d633
Nagendra <nagendrababu.marasu@gmail.com>
2023-09-27 11:09:55 +0530

    completed exercsie 6.0

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,12 +6,19 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import { PokemonForm, fetchPokemon, PokemonInfoFallback, PokemonDataView } from '../pokemon'
 
-function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
+function PokemonInfo({ pokemonName }) {
+  const [pokemon, setPokemon] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  React.useEffect(() => {
+    if (!pokemonName) {
+      return
+    }
+    setPokemon(null);
+    fetchPokemon(pokemonName).then(pokemonData => { setPokemon(pokemonData) })
+      .catch(error => setError(error));
+  }, [pokemonName]);
   // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
   // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
   // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null.
@@ -23,10 +30,20 @@ function PokemonInfo({pokemonName}) {
   // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
-
-  // 💣 remove this
-  return 'TODO'
+  //   3. pokemon: 
+  if (error) {
+    return <div role="alert">
+      There was an error: <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+    </div>
+  }
+  if (!pokemonName) {
+    return 'Submit a pokemon';
+  }
+  else if (!pokemon) {
+    return <PokemonInfoFallback name={pokemonName} />
+  } else {
+    return <PokemonDataView pokemon={pokemon} />
+  }
 }
 
 function App() {
